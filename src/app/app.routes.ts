7cfd2bd59f00f6f387402076867c6635bd684f51
Birka <birkavidit@gmail.com>
2025-09-03 13:22:58 +0200

    made article for article page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {Home} from './home/home';
+import {Adventures} from './adventures/adventures';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'adventures',
+    component: Adventures,
+  }
+];
