8dadd8c886208b034232521a5af8f4cdb5221810
Stefan Carpeliuc <stefan.carpeliuc@tible.com>
2025-12-07 18:15:39 +0100

    added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './components/navbar/navbar';
 import { StarfieldDirective } from './directives/starfield.directive';
+import {Footer} from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, StarfieldDirective],
+  imports: [RouterOutlet, Navbar, StarfieldDirective, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
